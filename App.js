d0b28aa9916aefca4c1eef59f5246b0cb9792989
Ostap Pashko <ostap.pashko@eleks.com>
2018-02-20 15:59:57 +0200

    updated component registry with redux store and Provider

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { Navigation } from "react-native-navigation";
+import { Provider } from "react-redux";
 
+import configureStore from "./src/store/configureStore";
+
+// Screens
 import AuthScreen from "./src/screens/Auth/Auth";
 import SharePlace from "./src/screens/SharePlace/SharePlace";
 import FindPlace from "./src/screens/FindPlace/FindPlace";
 
+const store = configureStore();
+
 // Register screens
-Navigation.registerComponent("AuthScreen", () => AuthScreen);
-Navigation.registerComponent("SharePlace", () => SharePlace);
-Navigation.registerComponent("FindPlace", () => FindPlace);
+Navigation.registerComponent("AuthScreen", () => AuthScreen, store, Provider);
+Navigation.registerComponent("SharePlace", () => SharePlace, store, Provider);
+Navigation.registerComponent("FindPlace", () => FindPlace, store, Provider);
 
 // Start an App
 Navigation.startSingleScreenApp({
