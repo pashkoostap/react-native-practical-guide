0d4d86402f541ded5448878552a1ef55bcb9c893
Ostap Pashko <ostap.pashko@eleks.com>
2018-02-23 13:04:26 +0200

    updated styles for SideDrawer component

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { View, Text, Dimensions, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 class SideDrawer extends Component {
   render() {
@@ -10,7 +17,12 @@ class SideDrawer extends Component {
           { width: Dimensions.get("window").width * 0.8 }
         ]}
       >
-        <Text>Side drawer</Text>
+        <TouchableOpacity>
+          <View style={styles.menuItem}>
+            <Icon name="ios-log-out" size={30} color="#000" />
+            <Text style={styles.menuItemText}>Log out</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -19,7 +31,18 @@ class SideDrawer extends Component {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
-    flex: 1
+    flex: 1,
+    paddingTop: 30
+  },
+  menuItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#eee"
+  },
+  menuItemText: {
+    marginLeft: 10,
+    color: "#000"
   }
 });
 
