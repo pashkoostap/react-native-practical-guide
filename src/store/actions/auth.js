922d51d6c75da074e4395d0faf05008b41c3c5bd
pashkoostap <pashkoostap@gmail.com>
2018-03-01 17:48:02 +0200

    created signUp functionality for the Auth screen

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,34 @@
 import { TRY_AUTH } from "./actionTypes";
+import { uiStartLoading, uiStopLoading } from "./ui";
+
+const apiURL =
+  "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBOc8srMkdFa1KCalkZj2jHv6ZkabRxq-I";
 
 export const tryAuth = authData => {
-  return {
-    type: TRY_AUTH,
-    authData
+  return dispatch => {
+    dispatch(authSignUp(authData));
+  };
+};
+
+export const authSignUp = authData => {
+  return dispatch => {
+    dispatch(uiStartLoading());
+
+    fetch(apiURL, {
+      method: "POST",
+      body: JSON.stringify({
+        ...authData,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        dispatch(uiStopLoading());
+        console.log(res);
+      })
+      .catch(err => alert("Authentication error"));
   };
 };
