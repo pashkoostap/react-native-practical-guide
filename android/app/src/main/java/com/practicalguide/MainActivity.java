be1edcf2f8370e4ef4f1c379f67906b0b081bc0d
pashkoostap <pashkoostap@gmail.com>
2018-03-06 16:38:41 +0200

    added splash screen configuration to the existing application

--- android/app/src/main/java/com/practicalguide/MainActivity.java
+++ android/app/src/main/java/com/practicalguide/MainActivity.java
@@ -1,8 +1,27 @@
 package com.practicalguide;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity  {
+    @Override
+    public LinearLayout createSplashLayout() {
+      LinearLayout view = new LinearLayout(this);
+      TextView textView = new TextView(this);
 
-    
+      view.setBackgroundColor(Color.parseColor("#521751"));
+      view.setGravity(Gravity.CENTER);
+
+      textView.setTextColor(Color.parseColor("#fa923f"));
+      textView.setText("React Native");
+      textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+      view.addView(textView);
+      return view;
+    }
 }
